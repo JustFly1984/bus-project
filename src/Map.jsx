ab093300ed4800a3ff4b2f45757f8f5e5c9dbd7e
Garrett Cambre <31525494+garrettcambre@users.noreply.github.com>
2020-09-20 20:32:06 -0400

    cleaning up code and commenting

--- src/Map.jsx
+++ src/Map.jsx
@@ -2,112 +2,133 @@
 import React, { Component } from "react";
 
 import { GoogleMap, useLoadScript, DirectionsService, DirectionsRenderer , DistanceMatrixService } from "@react-google-maps/api";
-import MapTest from './MapTest'
+import MapTest from './MapTest';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import TextField from '@material-ui/core/TextField';
+import {InputLabel, FilledInput} from '@material-ui/core/';
+
 
 // const { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } = require("../../");
 // const ScriptLoaded = require("../../docs/ScriptLoaded").default;
 
-class Map extends Component {
+function Map() {
+
+
+  //
+  // constructor (props) {
+  //   super(props)
+    //
+    // this.state = {
+    //   response: null,
+    //   travelMode: 'DRIVING',
+    //   origin: '',
+    //   destination: ''
+    // }
+      const [value, setValue] = React.useState('TRANSIT');
+
+      const [response, setDirections] = React.useState([]);
+
+      const [waypoints, updateWaypoints] = React.useState([{
+        origin:"",
+        destination:"",
+      }]);
+      // const [destination, updateDestination] = React.useState('');
+      //
+      // const [origin, updateOrigin] = React.useState('');
+      // const [travelMode, setTravelMode] = useState('');
+      // const [lastName, setLastname] = useState('');
+
+      // const logName = () => {
+        // do whatever with the names... let's just log them here
+      //   console.log(response);
+      //   console.log(firstName);
+      //   console.log(lastName);
+      // };
 
 
 
-  constructor (props) {
-    super(props)
 
-    this.state = {
-      response: null,
-      travelMode: 'DRIVING',
-      origin: '',
-      destination: ''
-    }
 
-    this.directionsCallback = this.directionsCallback.bind(this)
-    this.checkDriving = this.checkDriving.bind(this)
-    this.checkBicycling = this.checkBicycling.bind(this)
-    this.checkTransit = this.checkTransit.bind(this)
-    this.checkWalking = this.checkWalking.bind(this)
-    this.getOrigin = this.getOrigin.bind(this)
-    this.getDestination = this.getDestination.bind(this)
-    this.onClick = this.onClick.bind(this)
-    this.onMapClick = this.onMapClick.bind(this)
-  }
+    // this.directionsCallback = this.directionsCallback.bind(this)
+    // this.getOrigin = this.getOrigin.bind(this)
+    // this.getDestination = this.getDestination.bind(this)
+    // this.onClick = this.onClick.bind(this)
+    // this.onMapClick = this.onMapClick.bind(this)
 
-  directionsCallback (response) {
-    console.log(response)
 
-    if (response !== null) {
-      if (response.status === 'OK') {
-        this.setState(
-          () => ({
-            response
-          })
-        )
-      } else {
-        console.log('response: ', response)
-      }
-    }
-  }
+    const directionsCallback = () => {
+        if (response !== null) {
+          if (response.status === 'OK') {
+            setDirections(response);
+            console.log(response);
+            console.log('set directions')
+          } else {
+            console.log('response status: '+response.status  );
+            console.log('response: ', response );
+          }
+        }
+        console.log('null response')
+    };
 
-  checkDriving ({ target: { checked } }) {
-    checked &&
-      this.setState(
-        () => ({
-          travelMode: 'DRIVING'
-        })
-      )
-  }
 
-  checkBicycling ({ target: { checked } }) {
-    checked &&
-      this.setState(
-        () => ({
-          travelMode: 'BICYCLING'
-        })
-      )
-  }
 
-  checkTransit ({ target: { checked } }) {
-    checked &&
-      this.setState(
-        () => ({
-          travelMode: 'TRANSIT'
-        })
-      )
-  }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-  checkWalking ({ target: { checked } }) {
-    checked &&
-      this.setState(
-        () => ({
-          travelMode: 'WALKING'
-        })
-      )
-  }
 
-  getOrigin (ref) {
-    this.origin = ref
-  }
 
-  getDestination (ref) {
-    this.destination = ref
-  }
 
-  onClick () {
-    if (this.origin.value !== '' && this.destination.value !== '') {
-      this.setState(
-        () => ({
-          origin: this.origin.value,
-          destination: this.destination.value
-        })
-      )
-    }
-  }
+  // const getOrigin = () => {
+  //   this.origin = ref
+  // };
 
-  onMapClick (...args) {
-    console.log('onClick args: ', args)
-  }
+  // const getDestination = () => {
+  //   this.destination = ref
+  // };
+ const handleDestinationChange = (event) => {
+  // updateWaypoints [...waypoints, {origin: this.origin.value, destination: event.target.value}]
+  waypoints.destination = event.target.value
+  console.log(event.target.value);
+  console.log('handleDestinationChange');
+  console.log('waypoints.destination: '+waypoints.destination);
+ }
 
-  render () {
+ const handleOriginChange = (event) => {
+  // updateWaypoints [...waypoints, {origin: this.origin.value, destination: event.target.value}]
+  waypoints.origin = event.target.value
+  console.log(event.target.value);
+  console.log('handleoriginChange');
+  console.log('waypoints.origin: '+waypoints.origin);
+ }
+
+
+  // onClick () {
+  //   if (this.origin.value !== '' && this.destination.value !== '') {
+  //     this.setState(
+  //       () => ({
+  //         origin: this.origin.value,
+  //         destination: this.destination.value
+  //       })
+  //     )
+  //   }
+  // }
+const onClick =  () => {
+  // updateWaypoints([...waypoints, { origin: this.origin.value, destination: this.destination.value}]);
+
+};
+
+
+  //
+  // const onMapClick = () => {
+  //   console.log('onClick args: ', args)
+  // }
+
+  // render () {
     // const { isLoaded, loadError } = useLoadScript({
     //   googleMapsApiKey:"AIzaSyBdVcGctD6SNcD8sIJ7BbVNLwBat_fWHVo",
     //   // {...other options}
@@ -115,7 +136,9 @@ class Map extends Component {
 
     const { isLoaded } = useLoadScript({
   // Enter your own Google Maps API key
-  googleMapsApiKey: "AIzaSyBdVcGctD6SNcD8sIJ7BbVNLwBat_fWHVo"
+
+  googleMapsApiKey: "AIzaSyBdVcGctD6SNcD8sIJ7BbVNLwBat_fWHVo",
+
 });
 
 
@@ -128,6 +151,12 @@ class Map extends Component {
        //   }
        //  )
      const renderMap = () => {
+       console.log('rendering map')
+       console.log('origin: '+waypoints.origin)
+       console.log('desitnation: ' +waypoints.destination)
+       console.log('response: '+response)
+       console.log('value: '+value)
+       // console.log('isloaded: '+isloaded)
         return (
           <div className='map'>
             <div className='map-settings'>
@@ -138,7 +167,9 @@ class Map extends Component {
                   <div className='form-group'>
                     <label htmlFor='ORIGIN'>Origin</label>
                     <br />
-                    <input id='ORIGIN' className='form-control' type='text' ref={this.getOrigin} />
+                    <input id='ORIGIN' className='form-control' type='text'
+                     // ref={this.getOrigin}
+                     />
                   </div>
                 </div>
 
@@ -146,62 +177,46 @@ class Map extends Component {
                   <div className='form-group'>
                     <label htmlFor='DESTINATION'>Destination</label>
                     <br />
-                    <input id='DESTINATION' className='form-control' type='text' ref={this.getDestination} />
+                    <input id='DESTINATION' className='form-control' type='text'
+                     // ref={this.getDestination}
+                     />
                   </div>
                 </div>
+                <div>
+                <FormControl variant="filled">
+                 <InputLabel htmlFor="component-filled">Origin</InputLabel>
+
+                 <FilledInput id="component-filled"
+                  value={'10621 thistlewood ct'}
+                  onChange={handleOriginChange} />
+               </FormControl>
+
+                </div>
+                <br/>
+                  <div>
+                  <FormControl variant="filled">
+                   <InputLabel htmlFor="component-filled">Destination</InputLabel>
+
+                   <FilledInput id="component-filled"
+                    value={'9256 fidelis dr'}
+                    onChange={handleDestinationChange} />
+                 </FormControl>
+
+                  </div>
+                  <br/>
               </div>
+              <FormControl component="fieldset">
+                    <FormLabel component="legend">Gender</FormLabel>
+                    <RadioGroup aria-label="Trasport Mode" name="transportMode" value={value} onChange={handleChange}>
+                      <FormControlLabel value="DRIVING" control={<Radio />} label="DRIVING" />
+                      <FormControlLabel value="BICYCLING" control={<Radio />} label="BICYCLING" />
+                      <FormControlLabel value="TRANSIT" control={<Radio />} label="TRANSIT" />
+                      <FormControlLabel value="WALKING" control={<Radio />} label="WALKING" />
+                    </RadioGroup>
+                  </FormControl>
 
-              <div className='d-flex flex-wrap'>
-                <div className='form-group custom-control custom-radio mr-4'>
-                  <input
-                    id='DRIVING'
-                    className='custom-control-input'
-                    name='travelMode'
-                    type='radio'
-                    checked={this.state.travelMode === 'DRIVING'}
-                    onChange={this.checkDriving}
-                  />
-                  <label className='custom-control-label' htmlFor='DRIVING'>Driving</label>
-                </div>
 
-                <div className='form-group custom-control custom-radio mr-4'>
-                  <input
-                    id='BICYCLING'
-                    className='custom-control-input'
-                    name='travelMode'
-                    type='radio'
-                    checked={this.state.travelMode === 'BICYCLING'}
-                    onChange={this.checkBicycling}
-                  />
-                  <label className='custom-control-label' htmlFor='BICYCLING'>Bicycling</label>
-                </div>
-
-                <div className='form-group custom-control custom-radio mr-4'>
-                  <input
-                    id='TRANSIT'
-                    className='custom-control-input'
-                    name='travelMode'
-                    type='radio'
-                    checked={this.state.travelMode === 'TRANSIT'}
-                    onChange={this.checkTransit}
-                  />
-                  <label className='custom-control-label' htmlFor='TRANSIT'>Transit</label>
-                </div>
-
-                <div className='form-group custom-control custom-radio mr-4'>
-                  <input
-                    id='WALKING'
-                    className='custom-control-input'
-                    name='travelMode'
-                    type='radio'
-                    checked={this.state.travelMode === 'WALKING'}
-                    onChange={this.checkWalking}
-                  />
-                  <label className='custom-control-label' htmlFor='WALKING'>Walking</label>
-                </div>
-              </div>
-
-              <button className='btn btn-primary' type='button' onClick={this.onClick}>
+              <button className='btn btn-primary' type='button' onClick={onClick}>
                 Build Route
               </button>
             </div>
@@ -216,14 +231,14 @@ class Map extends Component {
                   width: '100%'
                 }}
                 // required
-                zoom={2}
+                zoom={6}
                 // required
                 center={{
                   lat: 0,
                   lng: -180
                 }}
                 // optional
-                onClick={this.onMapClick}
+                // onClick={onMapClick}
                 // optional
                 onLoad={map => {
                   console.log('DirectionsRenderer onLoad map: ', map)
@@ -235,19 +250,26 @@ class Map extends Component {
               >
                 {
                   (
-                    this.state.destination !== '' &&
-                    this.state.origin !== ''
+                    // this.state.destination !== '' &&
+                    // this.state.origin !== ''
+                    waypoints.destination !== '' &&
+                    waypoints.origin !== ''
                   ) && (
                     <DirectionsService
                       // required
                     /*global google  // eslint-disable-line react-perf/jsx-no-new-object-as-prop */
                       options={{
-                        destination: this.state.destination,
-                        origin: this.state.origin,
-                        travelMode: this.state.travelMode
+                        // destination: this.state.destination,
+                        destination: waypoints.destination,
+                        // origin: this.state.origin,
+                        origin:  waypoints.origin,
+                        // travelMode: this.state.travelMode
+                        travelMode: value,
                       }}
                       // required
-                      callback={this.directionsCallback}
+                      // callback={directionsCallback}
+                      callback = {(response) => {console.log('teh callback repsonse is '+ JSON.stringify(response)) ;
+                      console.log('got a response ds 272')}}
                       // optional
                       onLoad={directionsService => {
                         console.log('DirectionsService onLoad directionsService: ', directionsService)
@@ -261,11 +283,13 @@ class Map extends Component {
                 }
 
                 {
-                  this.state.response !== null && (
+                  // this.state.response !== null && (
+                  response !== null && (
                     <DirectionsRenderer
                       // required
                       options={{ /* // eslint-disable-line react-perf/jsx-no-new-object-as-prop*/
-                        directions: this.state.response
+                        // directions: this.state.response
+                        directions: response
                       }}
                       // optional
                       onLoad={directionsRenderer => {
@@ -280,20 +304,25 @@ class Map extends Component {
                 }
                 <DistanceMatrixService
                  options={{
-                   destination: this.state.destination,
-                   origin: this.state.origin,
-                   travelMode: this.state.travelMode
+                   // destination: this.state.destination,
+                   // origin: this.state.origin,
+                   // travelMode: this.state.travelMode
+                   destination: waypoints.destination,
+                   origin:  waypoints.origin,
+                   travelMode: value,
                          }}
-                 callback = {(response) => {console.log(response)}}
+                 callback = {(response) => {console.log('teh callback repsonse is '+response) ;
+                 console.log('got a response dms')}}
                 />
               </GoogleMap>
             </div>
           </div>
         );
       };
+      console.log('310')
       return isLoaded ? renderMap() : null;
 }
-}
+// }
 //
 // <ScriptLoaded>
 //   <Map />
